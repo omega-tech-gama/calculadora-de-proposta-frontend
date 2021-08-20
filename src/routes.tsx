b6afa1451243c6b343d709c4734605031702383f
Guilherme <guilherme@nfe.io>
2021-08-20 15:24:55 -0300

    propostas route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Create } from "./pages/Create";
 import { SignUp } from "./pages/SignUp";
+import { Proposals } from "./pages/Proposals";
 
 function Routes(){
   return (
       <BrowserRouter>
           <Route path="/" exact component={SignUp}/>
-          <Route path="/create" component={Create}/>
+          <Route path="/create" exact component={Create}/>
+          <Route path="/propostas" exact component={Proposals}/>
       </BrowserRouter>
   );
 }
