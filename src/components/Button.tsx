a94cb423414eb002f504f2f070a72bab74022896
Guilherme <guilherme@nfe.io>
2021-08-20 20:48:30 -0300

    refactor button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
+
 interface ButtonProps {
   style: string;
   icon?: boolean;
+  href?: string;
+  onClick?: () => void
 }
 
-export const Button: React.FC<ButtonProps> = ({ style, children, icon }) => (
-  <button className={`btn btn-${style} ${icon && 'p-2'}`}>
-    {children}
-  </button>
-)
+export const Button: React.FC<ButtonProps> = ({ style, children, icon, href, onClick }) => {
+
+  const className = `btn btn-${style} ${icon && 'p-2'}`;
+
+  if (href) {
+    return (
+      <Link to={href} className={className}>
+        {children}
+      </Link>
+    )
+  } else {
+    return (
+      <button onClick={onClick} className={className}>
+        {children}
+      </button>
+    )
+  }
+}
