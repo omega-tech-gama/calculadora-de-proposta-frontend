2c9aedb0d0d234774427a21ccf519607cc6d2a88
Guilherme <guilherme@nfe.io>
2021-08-20 16:28:01 -0300

    create loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className=" flex justify-center items-center transform translate-y-40">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-omega"></div>
+    </div>
+  )
+}
