6377fe9691af3267c504e74037833c04096ac3d2
Guilherme <guilherme@nfe.io>
2021-08-21 13:23:48 -0300

    routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,10 @@
-import { useContext } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { SignUp } from "./pages/SignUp";
 import { SignIn } from "./pages/SignIn";
 import { Proposals } from "./pages/Proposals";
-import { AuthContext } from "./context/AuthContext";
 import { Proposal } from "./pages/Proposal";
 
 function Routes() {
-  const { data } = useContext(AuthContext);
 
   return (
     <BrowserRouter>
