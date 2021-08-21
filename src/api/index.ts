fed55d574bc5ce03b27f0eb121cc3f2b6cca60a0
Guilherme <guilherme@nfe.io>
2021-08-21 13:22:00 -0300

    set authorization header

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { getToken } from "../context/AuthContext";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: "https://omega-backend-nestjs.herokuapp.com",  
 })
+
+api.interceptors.request.use(async config => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
